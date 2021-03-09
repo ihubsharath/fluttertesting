8580c54c49421d4ae69140630b95d94888720619
Sharath <sharathchandrapsn@gmail.com>
2021-03-09 18:30:16 +0530

    changed title in branch clear

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Branch',
+      title: 'Flutter Demo Branch Testing',
       theme: ThemeData(
         // This is the theme of your application.
         //
